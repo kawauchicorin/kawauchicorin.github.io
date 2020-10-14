68b56206208ebcc3de58f75f68c993152873ce16
CORIN008 <corin008@MacBook-Pro.local>
2020-10-14 12:01:04 +0900

    edit signaling_room.js

--- nodejss/signaling_room.js
+++ nodejss/signaling_room.js
@@ -1,16 +1,52 @@
 "use strict";
 
-var srv = require('http').Server(); //moduleの読み込み
-var io = require('socket.io')(srv); //moduleの読み込み
+// Please install socket.io module
+//   npm install socket.io
+//
+// Run
+//   node signaling_room.js
+
+
+
+/*--
+let WebSocketServer = require('ws').Server;
+let port = 3001;
+let wsServer = new WebSocketServer({ port: port });
+console.log('websocket server start. port=' + port);
+wsServer.on('connection', function(ws) {
+  console.log('-- websocket connected --');
+  ws.on('message', function(message) {
+    wsServer.clients.forEach(function each(client) {
+      if (isSame(ws, client)) {
+        console.log('- skip sender -');
+      }
+      else {
+        client.send(message);
+      }
+    });
+  });
+});
+function isSame(ws1, ws2) {
+  // -- compare object --
+  return (ws1 === ws2);
+  // -- compare undocumented id --
+  //return (ws1._ultron.id === ws2._ultron.id);
+}
+--*/
+
+
+var srv = require('http').Server();
+var io = require('socket.io')(srv);
 var port = 3002;
 srv.listen(port);
 console.log('signaling server started on port:' + port);
+
 
 // This callback function is called every time a socket
 // tries to connect to the server
 io.on('connection', function(socket) {
     // ---- multi room ----
-    socket.on('enter', function(roomname) { //入室要求がきたらルームにjoinさせる
+    socket.on('enter', function(roomname) {
       socket.join(roomname);
       console.log('id=' + socket.id + ' enter room=' + roomname);
       setRoomname(roomname);
@@ -25,16 +61,16 @@ io.on('connection', function(socket) {
       return room;
     }
 
-    function emitMessage(type, message) { // ルーム内の全員に送る場合
+    function emitMessage(type, message) {
       // ----- multi room ----
       var roomname = getRoomname();
 
       if (roomname) {
-        console.log('===== message broadcast to room -->' + roomname);  // ルーム内に送る
+        //console.log('===== message broadcast to room -->' + roomname);
         socket.broadcast.to(roomname).emit(type, message);
       }
       else {
-        console.log('===== message broadcast all'); // ルーム未入室の場合は、全体に送る
+        console.log('===== message broadcast all');
         socket.broadcast.emit(type, message);
       }
     }
@@ -44,12 +80,12 @@ io.on('connection', function(socket) {
     socket.on('message', function(message) {
         var date = new Date();
         message.from = socket.id;
-        console.log(date + 'id=' + socket.id + ' Received Message: ' + JSON.stringify(message));
+        //console.log(date + 'id=' + socket.id + ' Received Message: ' + JSON.stringify(message));
 
         // get send target
         var target = message.sendto;
         if (target) {
-          console.log('===== message emit to -->' + target);
+          //console.log('===== message emit to -->' + target);
           socket.to(target).emit('message', message);
           return;
         }
